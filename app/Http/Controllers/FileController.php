dab5ae06d0d3eb42231e3f8ca75081308299d8b5
luantm16110560 <43678069+luantm16110560@users.noreply.github.com>
2019-11-02 00:16:20 +0700

    resize image when upload and input image exception

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Support\Facades\Validator;
 use DB;
 use Carbon;
 class FileController extends Controller
@@ -13,28 +15,30 @@ class FileController extends Controller
     }
     public function storeFile(request $request)
     {
-       
-        if($request->hasFile('file'))
-        {
-           
-           
+
+        if($request->hasFile('file')) {
+
             $myDate = Carbon\Carbon::now()->toDateString();
 
             $myTime =  Carbon\Carbon::now()->hour."h_".Carbon\Carbon::now()->minute."m_".Carbon\Carbon::now()->second."s";
-
+    
             $myFile=$request->file->getClientOriginalName();
 
             $filename =$myDate."_".$myTime."_".$myFile;
+    
 
-            //echo $filename;
-           $request->file->storeAs('image/slide',$filename); 
-       
+
+
+            $image       = $request->file('file');
+            $image_resize = Image::make($image->getRealPath());              
+           // $image_resize->resize(270, 320);
+            $image_resize->save(public_path('source/image/slide/'.$filename));
 
             DB::table('slides')->insert(
                 ['image' => $filename, 'status'=>1]
             );
-        
-        }
+         
+         }
  
      
     }
--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Intervention\Image\ImageServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -189,6 +189,8 @@ return [
     */
 
     'aliases' => [
+
+        'Image' => Intervention\Image\Facades\Image::class,
 
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
