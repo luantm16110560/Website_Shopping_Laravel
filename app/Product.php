ce462c534bdf8d3305cdc3a45bbbc8ea2fcb8e8c
luantm16110560 <43678069+luantm16110560@users.noreply.github.com>
2019-11-01 17:17:53 +0700

    add atribute to table products and model product

--- app/Product.php
+++ app/Product.php
@@ -8,7 +8,7 @@ class Product extends Model
 {
     protected $table='products';
 
-    protected $fillable=['name','description','unit_price','promotion_price','amount','image','status','id_type'];
+    protected $fillable=['name','description','unit_price','promotion_price','amount','image','isNew','status','id_type'];
 
     public $timestamps=false;
 
