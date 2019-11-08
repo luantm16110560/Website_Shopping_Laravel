1e4ca5e63405ed6233a29c94899871ce11e5a520
Duc Quy <quyducnguyen2210@gmail.com>
2019-11-08 16:14:24 +0700

    update_gioithieu

--- resources/views/page/gioithieu.blade.php
+++ resources/views/page/gioithieu.blade.php
@@ -203,43 +203,39 @@
     <div style="border: 1px; border-color:blue; border-style: groove;"></div>
     <div class="space50">&nbsp;</div>
 
-    <h2 class="text-center wow fadeInDownwow fadeInDown">Our Amaizing Team</h2>
+    <h2 class="text-center wow fadeInDownwow fadeInDown">Đội Ngũ Tuyệt Vời Của Chúng Tôi</h2>
     <div class="space20">&nbsp;</div>
-    <h5 class="text-center other-title wow fadeInLeft">Founders</h5>
-    <p class="text-center wow fadeInRight">Nemo enims voluptatem quia volupas sit aspe aut odit aut fugit, sed quia <br />consequuntur magni dolores.</p>
+    <h5 class="text-center other-title wow fadeInLeft">Người Sáng Lập</h5>
+    <p class="text-center wow fadeInRight">Chúng tôi là những người đồng sáng lập nên <strong style="color: #000099">TOM'S SHOES</strong> <br />Với chúng tôi không gì là không thể</p>
     <div class="space20">&nbsp;</div>
     <div class="row">
       <div class="col-sm-6 wow fadeInLeft">
-        <div class="beta-person media">
-        
+        <div class="bets-img-hover">
           <img class="pull-left" src="source/assets/dest/images/person2.jpg" alt="">
-        
           <div class="media-body beta-person-body">
-            <h5>Bob Robertson</h5>
+            <h5>Mr.Duc Quy</h5>
             <p class="font-large">Founder</p>
             <p>Nemo enim ipsam voluptatem quia voluptas sit asatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque por quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam.</p>
-            <a href="single_type_gallery.html">View projects <i class="fa fa-chevron-right"></i></a>
+            <a href="single_type_gallery.html">View profile <i class="fa fa-chevron-right"></i></a>
           </div>
         </div>
       </div>
       <div class="col-sm-6 wow fadeInRight">
-        <div class="beta-person media ">
-        
+        <div class="bets-img-hover">
           <img class="pull-left" src="source/assets/dest/images/person3.jpg" alt="">
-        
           <div class="media-body beta-person-body">
-            <h5>Mike Greenwood</h5>
-            <p class="font-large">Founder</p>
+            <h5>Mr.Minh Luan</h5>
+            <p class="font-large">President</p>
             <p>Nemo enim ipsam voluptatem quia voluptas sit asatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque por quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam.</p>
-            <a href="single_type_gallery.html">View projects <i class="fa fa-chevron-right"></i></a>
+            <a href="single_type_gallery.html">View profile <i class="fa fa-chevron-right"></i></a>
           </div>
         </div>
       </div>
     </div>
     
     <div class="space60">&nbsp;</div>
-    <h5 class="text-center other-title wow fadeInDown">The Best of Professionals</h5>
-    <p class="text-center wow fadeInUp">Nemo enims voluptatem quia volupas sit aspe aut odit aut fugit, sed quia <br />consequuntur magni dolores.</p>
+    <h5 class="text-center other-title wow fadeInDown">Người Đồng Hành</h5>
+    <p class="text-center wow fadeInUp">Chúng tôi là những nhân viên ưu tú của <strong style="color: #000099">TOM'S SHOES</strong><br />Với chúng tôi <strong style="color: #000099">TOM'S SHOES</strong> là nhà</p>
     <div class="space20">&nbsp;</div>
     <div class="row">
       <div class="col-sm-3">
@@ -248,10 +244,10 @@
           <img src="source/assets/dest/images/person1.jpg" alt="">
       </div>
           <div class="beta-person-body">
-            <h5>Mark Priston</h5>
-            <p class="font-large">Web developer</p>
+            <h5>Mr.Duc Anh</h5>
+            <p class="font-large">Sale Manager</p>
             <p>Nemo enims voluptatem quia volupas sit aspe aut odit aut fugit, sed quia consequuntur magni dolores.</p>
-            <a href="single_type_gallery.html">View projects <i class="fa fa-chevron-right"></i></a>
+            <a href="single_type_gallery.html">View profile <i class="fa fa-chevron-right"></i></a>
           </div>
         </div>
       </div>
@@ -261,10 +257,10 @@
           <img src="source/assets/dest/images/person2.jpg" alt="">
         </div>
           <div class="beta-person-body">
-            <h5>Bob Robertson</h5>
-            <p class="font-large">Web developer</p>
+            <h5>Mrs.Yen Nhi</h5>
+            <p class="font-large">Marketing Manager</p>
             <p>Nemo enims voluptatem quia volupas sit aspe aut odit aut fugit, sed quia consequuntur magni dolores.</p>
-            <a href="single_type_gallery.html">View projects <i class="fa fa-chevron-right"></i></a>
+            <a href="single_type_gallery.html">View profile <i class="fa fa-chevron-right"></i></a>
           </div>
         </div>
       </div>
@@ -274,10 +270,10 @@
           <img src="source/assets/dest/images/person3.jpg" alt="">
         </div>
           <div class="beta-person-body">
-            <h5>Mike Greenwood</h5>
-            <p class="font-large">Web developer</p>
+            <h5>Mr.Duy Hung</h5>
+            <p class="font-large">Market Manager</p>
             <p>Nemo enims voluptatem quia volupas sit aspe aut odit aut fugit, sed quia consequuntur magni dolores.</p>
-            <a href="single_type_gallery.html">View projects <i class="fa fa-chevron-right"></i></a>
+            <a href="single_type_gallery.html">View profile <i class="fa fa-chevron-right"></i></a>
           </div>
         </div>
       </div>
@@ -287,10 +283,10 @@
           <img src="source/assets/dest/images/person4.jpg" alt="">
         </div>
           <div class="beta-person-body">
-            <h5>David Black</h5>
-            <p class="font-large">Web developer</p>
+            <h5>Mrs.Bang Tranh</h5>
+            <p class="font-large">Customers Manager</p>
             <p>Nemo enims voluptatem quia volupas sit aspe aut odit aut fugit, sed quia consequuntur magni dolores.</p>
-            <a href="single_type_gallery.html">View projects <i class="fa fa-chevron-right"></i></a>
+            <a href="single_type_gallery.html">View profile <i class="fa fa-chevron-right"></i></a>
           </div>
         </div>
       </div>
