b657bf789ce5747a01291b24eac1c6c15ef82f97
Duc Quy <quyducnguyen2210@gmail.com>
2019-11-08 16:20:42 +0700

    update_nhungggmap

--- resources/views/page/lienhe.blade.php
+++ resources/views/page/lienhe.blade.php
@@ -15,7 +15,7 @@
 </div>
 <div class="beta-map">
   
-  <div class="abs-fullwidth beta-map wow flipInX"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3678.0141923553406!2d89.551518!3d22.801938!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff8ff8ef7ea2b7%3A0x1f1e9fc1cf4bd626!2sPranon+Pvt.+Limited!5e0!3m2!1sen!2s!4v1407828576904" ></iframe></div>
+  <div class="abs-fullwidth beta-map wow flipInX"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9685.766328987373!2d106.76551660727917!3d10.84763396358898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175270ad28d48ab%3A0xa6c02de0a7c40d6c!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBTxrAgUGjhuqFtIEvhu7kgVGh14bqtdCBUUC4gSOG7kyBDaMOtIE1pbmg!5e0!3m2!1svi!2s!4v1573204766477!5m2!1svi!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe></div>
 </div>
 <div class="container">
   <div id="content" class="space-top-none">
