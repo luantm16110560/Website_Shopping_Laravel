d62a2cf0bb11a356fc18f3f0931bbd4954cf1277
luantm16110560 <43678069+luantm16110560@users.noreply.github.com>
2019-11-09 12:57:34 +0700

    update title web and icon web

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel </title>
+    <title>Website bán giày</title>
     <base href="{{asset('')}}">
+    <link rel="icon" href="{!! asset('source/image/icon/icon-web-title.png') !!}"/>
     <link href='http://fonts.googleapis.com/css?family=Dosis:300,400' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
