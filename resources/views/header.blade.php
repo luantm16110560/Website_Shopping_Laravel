e45ca7199d115502446819dd948deeefe1100912
luantm16110560 <43678069+luantm16110560@users.noreply.github.com>
2019-11-24 16:56:58 +0700

    fix interface shopping cart

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -17,57 +17,7 @@
           <div class="pull-right beta-components space-left ov">
              <div class="space10">&nbsp;</div>
              <div class="beta-comp">
-                @if(Session::has('cart'))
-                <div class="cart">
-                   <div class="beta-select"><strong class="fa fa-shopping-cart" aria-hidden="true" style="font-size:15px"></strong>  <strong>Giỏ Hàng (@if(Session::has('cart')){{Session('cart')->totalQty}}@else 0 @endif sản phẩm)</strong><i class="fa fa-chevron-down"></i></div>
-                   <div class="beta-dropdown cart-body">
-                      @foreach ($product_cart as $productcart)
-                      <div class="cart-item">
-                         <a class="cart-item-delete" href="{{route('deletegiohang',$productcart['item']['id'])}}"><i class="fa fa-times"></i></a>
-                         <div class="media">
-                            <a class="pull-left"  href="{{route('product-detail',$productcart['item']['id'])}}"><img src="source/image/product/{{$productcart['item']['image']}}" alt="" height="30px"></a>
-                            <div class="media-body">
-                               <span class="cart-item-title"><strong>{{$productcart['item']['name']}}</strong></span>                    
-                               <table >
-                                  <tr>
-                                     <th >Số lượng: 
-                                     </th>
-                                     <th style="color: white" class="text-center"> Nhân:
-                                     </th>
-                                     <th style="width:50%">Đơn giá:
-                                     </th>
-                                  </tr>
-                                  <tr>
-                                     <td class="text-center">
-                                        <span class="cart-item-amount">
-                                           {{$productcart['qty']}}
-                                     </td>
-                                     <td class="text-center">
-                                     <p>X</p>
-                                     </td>
-                                     <td >
-                                     <span class="cart-total-value"> 
-                                        @if(($productcart['item']['promotion_price'])==0){{number_format($productcart['item']['unit_price'])}} VND 
-                                        @else {{number_format($productcart['item']['promotion_price'])}}
-                                        @endif </span></span>
-                                     </td>
-                                  </tr>
-                               </table>
-                            </div>
-                         </div>
-                      </div>
-                      @endforeach
-                      <div class="cart-caption">
-                         <div class="cart-total text-right"><strong>Tổng tiền:</strong> <span class="cart-total-value">{{number_format(Session('cart')->totalPrice)}} VND</span></div>
-                         <div class="clearfix"></div>
-                         <div class="center">
-                            <div class="space10">&nbsp;</div>
-                            <a href="{{route("dathang")}}" class="beta-btn primary text-center" style="background: #000099; "><strong style="color: white">Tiến hành thanh toán</strong> <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
-                         </div>
-                      </div>
-                   </div>
-                </div>
-                @endif
+              
              </div>
           </div>
           <div class="clearfix"></div>
@@ -91,8 +41,62 @@
              </li>
              <li><a href="{{route("about")}}" style="font-size:18px; color:white; margin-top: 13px">Giới thiệu</a></li>
              <li><a href="{{route("contact")}}" style="font-size:18px; color:white; margin-top: 13px">Liên hệ</a></li>
+             
        </div>
-       <div class="col-sm-3"></div>
+     
+       <div class="col-sm-3" style="margin-top: 19px;">
+         @if(Session::has('cart'))
+         <div class="cart">
+            <div class="beta-select"><strong style="color: white" class="fa fa-shopping-cart" aria-hidden="true" style="font-size:15px"></strong> &nbsp <strong style="color: white">Giỏ Hàng (@if(Session::has('cart')){{Session('cart')->totalQty}}@else 0 @endif sản phẩm)</strong><i style="color: white" class="fa fa-chevron-down"></i></div>
+            <div class="beta-dropdown cart-body">
+               @foreach ($product_cart as $productcart)
+               <div class="cart-item">
+                  <a class="cart-item-delete" href="{{route('deletegiohang',$productcart['item']['id'])}}"><i class="fa fa-times"></i></a>
+                  <div class="media">
+                     <a class="pull-left"  href="{{route('product-detail',$productcart['item']['id'])}}"><img src="source/image/product/{{$productcart['item']['image']}}" alt="" height="30px"></a>
+                     <div class="media-body">
+                        <span class="cart-item-title"><strong>{{$productcart['item']['name']}}</strong></span>                    
+                        <table >
+                           <tr>
+                              <th >Số lượng: 
+                              </th>
+                              <th style="color: white" class="text-center"> Nhân:
+                              </th>
+                              <th style="width:50%">Đơn giá:
+                              </th>
+                           </tr>
+                           <tr>
+                              <td class="text-center">
+                                 <span class="cart-item-amount">
+                                    {{$productcart['qty']}}
+                              </td>
+                              <td class="text-center">
+                              <p>X</p>
+                              </td>
+                              <td >
+                              <span class="cart-total-value"> 
+                                 @if(($productcart['item']['promotion_price'])==0){{number_format($productcart['item']['unit_price'])}} VND 
+                                 @else {{number_format($productcart['item']['promotion_price'])}}
+                                 @endif </span></span>
+                              </td>
+                           </tr>
+                        </table>
+                     </div>
+                  </div>
+               </div>
+               @endforeach
+               <div class="cart-caption">
+                  <div class="cart-total text-right"><strong>Tổng tiền:</strong> <span class="cart-total-value">{{number_format(Session('cart')->totalPrice)}} VND</span></div>
+                  <div class="clearfix"></div>
+                  <div class="center">
+                     <div class="space10">&nbsp;</div>
+                     <a href="{{route("dathang")}}" class="beta-btn primary text-center" style="background: #000099; "><strong style="color: white">Tiến hành thanh toán</strong> <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+                  </div>
+               </div>
+            </div>
+         </div>
+         @endif
+       </div>
        <div class="col-sm-3">
      
        <form style="margin-top: 19px;" role="search" method="get" id="searchform" action="{{route('searchView')}}" autocomplete="off">
